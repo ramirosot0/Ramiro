b3fe1b4d4a4020eda9cbca2ea4b5dea4bd8ed409
Ramiro Soto <tkokoloko@gmail.com>
2019-10-01 09:22:36 -0700

    last file! ;

--- public/Homeworks/Project 2: JavaScript and jQuery/js.js
+++ public/Homeworks/Project 2: JavaScript and jQuery/js.js
@@ -2,27 +2,66 @@ $(document).ready(function() {
 
     $("button").on("click", buyNow);
 
-    let q1Response = $("#quest1").val();
+    let shirtPrice = 5.99;
+    let jeansPrice = 10.99;
+    let tax = 1.48;
+    let totalPrice = 0;
 
 
 
     function buyNow() {
+        let q1Response = $("#quest1").val();
+        let q2Response = $("#quest2").val();
 
-        if (q1Response == "on"){
-            $("#checkOut").html("correct one");
-            $("#checkOut").attr("class", "bg-warning text-white");
+        if (q1Response == "1"){
+           totalPrice += shirtPrice;
         }
-        else if (q1Response == "tw"){
-            $("#checkOut").html("correct two");
-            $("#checkOut").attr("class", "bg-warning text-white");
+        else if (q1Response == "2"){
+            totalPrice = shirtPrice * 2;
         }
-        else{
-            $("#checkOut").html("correct lsefinal");
-            $("#checkOut").attr("class", "bg-warning text-white");
+        else if (q1Response == "3"){
+            totalPrice = shirtPrice * 3;
         }
+        else if (q1Response == "4"){
+            totalPrice = shirtPrice * 4
+        }
+        else {
+            $("#tCero").html("No shirts were selected!");
+            $("#tCero").attr("class", "bg-secondary text-black");
+        }
+
+        if (q2Response == "1"){
+            totalPrice += jeansPrice;
+        }
+        else if (q2Response == "2"){
+            totalPrice = jeansPrice * 2;
+        }
+        else if (q2Response == "3"){
+            totalPrice = jeansPrice * 3;
+        }
+        else if (q2Response == "4"){
+            totalPrice = jeansPrice * 4;
+        }
+        else {
+            $("#jCero").html("No jeans were selected");
+            $("#jCero").attr("class", "bg-secondary text-black");
+        }
+
+        if(totalPrice != 0){
+            $("#checkOut").html("Order Summary");
+            $("#checkOut1").html("Items:  $"+totalPrice);
+            $("#checkOut2").html("Shipping & handling:$0.00");
+            $("#checkOu3").html("Total before tax: " + tax);
+
+
+
+        }
+
+
+
 
 
 
     }
 
-})
+});
